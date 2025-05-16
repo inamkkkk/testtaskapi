3eb5c433a0d441251dd64fe4c962fcf7d4e9e0be
inamkkkk <inamk9025@gmail.com>
2025-05-17 01:31:32 +0500

    passwords are protected

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ router.post('/login', async (req, res) => {
     console.log(req.body);
     const { email, password } = req.body;
     try {
-        let user = await User.findOne({ email });
+        let user = await User.findOne({ email }).select('-password');
         if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
 
         const isMatch = await bcrypt.compare(password, user.password);
