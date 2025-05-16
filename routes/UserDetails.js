ef815a2543dabf6c1f1d329664d98cb20931c7c2
inamkkkk <inamk9025@gmail.com>
2025-05-17 03:12:25 +0500

    user detail

--- routes/UserDetails.js
+++ routes/UserDetails.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User'); // adjust path as needed
+const User = require('../models/user'); // adjust path as needed
 const router = express.Router();
 
 
