3d086e5f944e902c8e2c6b633dead72359448d9b
inamkkkk <inamk9025@gmail.com>
2025-05-17 17:38:27 +0500

    new changes are pushed

--- routes/auth.js
+++ routes/auth.js
@@ -31,7 +31,7 @@ router.post('/login', async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
 
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '2m' });
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.json({ user:user,token });
     } catch (err) {
         res.status(500).send('Server error', err);
